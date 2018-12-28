0d12efff4752966b851f1a5289c409c87eadb33d
SulekhRanjan <sulekhranjan20@gmail.com>
2018-12-29 00:23:23 +0530

    this is commit of 2nd method

--- onlineshopping/src/main/java/com/LandT/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/LandT/onlineshopping/controller/PageController.java
@@ -2,6 +2,7 @@ package com.LandT.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -13,6 +14,14 @@ public class PageController {
 		modelAndView.addObject("greeting", "welcome page");
 		return modelAndView;
 
+	}
+	
+	@RequestMapping(value = {"/request"})
+	public ModelAndView getRequest(@RequestParam("greetings") String greetings) {
+		
+		ModelAndView modelAndView = new ModelAndView("page");
+		modelAndView.addObject("greeting", "welcome page");
+		return modelAndView;
 	}
 
 }
